c734d96265dadb0b0040ac55daf35dcaff76ca48
Mehefuj Ali <mehefujali30@gmail.com>
2024-10-02 00:21:33 +0530

    cart validation  created

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const loadData = () => {
       fetch('https://fakestoreapiserver.reactbd.com/amazonproducts')
             .then(res => res.json())
-            .then(data => displayData(data))
+            .then(data => {
+                  allAlbums = data
+                  displayData(data)
+            })
 }
 const displayData = (data) => {
       const cardContainer = document.getElementById('cardContainer')
@@ -29,16 +32,64 @@ const displayData = (data) => {
 
 }
 let countProd = 0
-
+let totalCartPrice = 0
+let cart = []
 const prductCounter = document.getElementById('prductCounter')
+const totalPrice = document.getElementById('totalPrice')
+const totalItems = document.getElementById('totalProducts')
+const vewCart = document.getElementById('carts')
 const addCart = (id) => {
-      countProd += 1
-      console.log(id);
-      prductCounter.innerText = countProd
+      const datanew = allAlbums.find(album => album.id === id)
+
+      const cartFind = cart.find(fcart => fcart.id === datanew.id)
+
+      if (!cartFind) {
+            cart.push(datanew)
+            console.log(cart);
+            prductCounter.innerText = cart.length
+            totalCartPrice += datanew.price
+            totalPrice.innerText = totalCartPrice.toFixed(2)
+            totalItems.innerText = cart.length
+            const cartCard = document.createElement('div')
+            cartCard.innerHTML = `
+            <div class="card card-side bg-base-100 shadow-xl">
+               <figure>
+                        <img
+             src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                    alt="Movie" />
+                </figure>
+                      <div class="card-body">
+              <h2 class="card-title">New movie is released!</h2>
+                    <p>Click the button to watch on Jetflix app.</p>
+                 <div class="card-actions justify-end">
+                      <button class="btn btn-primary">Watch</button>
+                </div>
+            </div>
+          </div>
+            `
+
+
+            vewCart.appendChild(cartCard)
+
+
+
+      }
+      else {
+            alert('cart already added')
+
+      }
+
+
+
 
 
 
 }
+document.getElementById('vewCart').addEventListener('click', () => {
+      console.log('clicked');
+      document.getElementById('cardContainer').classList.add('hidden')
 
+      vewCart.classList.remove('hidden')
+})
 
 loadData()
